c108712f7771ef7a421e11d61e6d2b6b5e5136d7
kok-s0s <2694308562@qq.com>
2022-05-31 10:32:20 +0800

    feat: use dialog to create a modal

---
+++ html/modals/script.js
@@ -0,0 +1,7 @@
+document.getElementById('open-dialog').addEventListener('click', () => {
+  document.querySelector('dialog').showModal();
+});
+
+// document.querySelector('dialog > button').addEventListener('click', () => {
+//   document.querySelector('dialog').close();
+// });
