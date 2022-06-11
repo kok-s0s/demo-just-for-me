e7589a95dd2cd32a9f8c72ce8aef320971d95522
kok-s0s <2694308562@qq.com>
2022-06-11 16:53:35 +0800

    fix: make sure get data to display

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-import data from './data.json' assert { type: 'json' }
-
-const listData = data[0].contents
-
-const generate = () => {
-  return (cards.innerHTML = listData
-    .map((item) => {
-      const { name, contents } = item
-      return `
-        <div class="card">
-          <div class="title">${name}</div>
-          <div class="list">
-            ${contents
-              .map((item) => {
-                return `<div class="item">${item.name}</div>`
-              })
-              .join('')}
+fetch('./data.json')
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json)
+    return (cards.innerHTML = json[0].contents
+      .map((item) => {
+        const { name, contents } = item
+        return `
+          <div class="card">
+            <div class="title">${name}</div>
+            <div class="list">
+              ${contents
+                .map((item) => {
+                  return `<div class="item">${item.name}</div>`
+                })
+                .join('')}
+            </div>
           </div>
-        </div>
-      `
-    })
-    .join(''))
-}
-
-generate()
+        `
+      })
+      .join(''))
+  })
