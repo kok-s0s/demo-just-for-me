c4f73078308441e663a555fd5811228424e60a3e
kok-s0s <2694308562@qq.com>
2022-06-12 11:58:00 +0800

    feat: add scrolling animation

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 fetch('./data.json')
   .then((response) => response.json())
   .then((json) => {
-    console.log(json)
     return (cards.innerHTML = json[0].contents
       .map((item) => {
         const { name, contents } = item
@@ -24,3 +23,33 @@ fetch('./data.json')
       })
       .join(''))
   })
+
+const scrollElements = document.querySelectorAll('.js-scroll')
+
+const elementInView = (el, percentageScroll = 100) => {
+  const elementTop = el.getBoundingClientRect().top
+
+  return elementTop <= (window.innerHeight || document.documentElement.clientHeight) * (percentageScroll / 100)
+}
+
+const displayScrollElement = (element) => {
+  element.classList.add('scrolled')
+}
+
+const hideScrollElement = (element) => {
+  element.classList.remove('scrolled')
+}
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 100)) {
+      displayScrollElement(el)
+    } else {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener('scroll', () => {
+  handleScrollAnimation()
+})
