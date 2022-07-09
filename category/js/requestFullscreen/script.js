4114f0355e0c103ba70b059646baf2a9a594edfe
kok-s0s <2694308562@qq.com>
2022-07-09 23:08:31 +0800

    feat: requestFullscreen

---
+++ category/js/requestFullscreen/script.js
@@ -0,0 +1,5 @@
+const gril = document.getElementById('girl')
+const gundam = document.getElementById('gundam')
+
+gril.addEventListener('click', () => gril.requestFullscreen())
+gundam.addEventListener('click', () => gundam.requestFullscreen())
